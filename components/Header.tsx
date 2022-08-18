afb91fa457795ca4ecb782f760277e9d377053b2
Kevin Kidd <harrison.kevin@protonmail.com>
2022-08-18 17:34:17 -0400

    refactor(Header): update component structure and types for better readability and maintainability

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,7 @@
-import {FunctionComponent, useEffect, useState} from "react";
+import { FunctionComponent, ReactElement, useEffect, useState } from "react";
 import { useMenuStore} from "../stores/Menu";
-import { HeaderProps } from "../types/general";
 
-const Header: FunctionComponent<HeaderProps> = ({ items }) => {
+const Header: FunctionComponent<{ items: ReactElement[] }> = ({ items }) => {
 
   const toggleMenu = useMenuStore((state) => state.toggleMenu);
   const [darkHeader, setDarkHeader] = useState<boolean>(true);
@@ -24,20 +23,24 @@ const Header: FunctionComponent<HeaderProps> = ({ items }) => {
     }, [darkHeader]);
 
   return (
-    <div className={`px-2 sm:px-10 top-0 z-20 py-4 lg:py-5 2xl:py-6 sticky transition duration-300 flex ${darkHeader ? "bg-transparent" : "bg-[#1A2128]"}`}>
-        <button type="button" onClick={() => toggleMenu()} className="text-white focus:outline-none lg:hidden ml-2 sm:ml-6">
-          <span className="sr-only">Open sidebar</span>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
-        <div className="flex-1 flex justify-end items-center lg:mr-10 md:mr-2 sm:mr-6 h-10 sm:h-12 2xl:h-14">
-          {
-              items.map((Item, index: number) => (
-                  <Item key={`header-item-${index}`} />
-              ))
-          }
-        </div>
+    <div className={`px-2 top-0 z-20 py-2 sm:py-4 w-full sticky flex flex-row transition duration-300 ${darkHeader ? "bg-transparent" : "bg-[#1A2128]"}`}>
+            <button type="button" onClick={() => toggleMenu()} className="text-white focus:outline-none lg:hidden ml-2 sm:ml-6">
+                <span className="sr-only">Open sidebar</span>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 sm:h-7 sm:w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+            </button>
+            <div className="lg:container lg:mx-auto lg:pr-20 w-full">
+                <div className="w-full flex justify-end items-center h-10">
+                    {
+                        items.map((Item: ReactElement, index: number) => (
+                            <div className="h-full sm:px-4 px-1 py-1 sm:py-0" key={`header-item-${index}`}>
+                                { Item }
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
       </div>
   )
 };
