24939e6b93ec3551621888451a7804e5a7124019
Kevin Kidd <harrison.kevin@protonmail.com>
2022-08-18 17:34:17 -0400

    refactor(ListingsSection): remove unused import and update class names

--- components/ListingsSection.tsx
+++ components/ListingsSection.tsx
@@ -1,6 +1,5 @@
-import { FunctionComponent, useEffect, useState } from "react";
+import { FunctionComponent, useState } from "react";
 import { useHomeStore } from "../stores/Home";
-import exampleListings from "../exampleListings.json";
 import { ListingCard } from "./ListingCard";
 import Typed from "react-typed";
 import {Listing} from "../types/general";
@@ -22,7 +21,7 @@ const DenomSearchBox: FunctionComponent<{type: string}> = ({ type }) => {
     return (
         <div className="search-box w-full h-full bg-[#0E1E2C] group">
             <div className="inline-flex flex w-full h-full items-center ml-5">
-                <svg xmlns="http://www.w3.org/2000/svg" className="big:h-6 big:h-12 lg:h-5 lg:w-5 sm:h-4 sm:w-4 h-3 w-3" fill="#B2BFCD" viewBox="0 0 512 512">
+                <svg xmlns="http://www.w3.org/2000/svg" className="2xl:h-6 2xl:w-6 lg:h-5 lg:w-5 h-4 w-4" fill="#B2BFCD" viewBox="0 0 512 512">
                     <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/>
                 </svg>
                 { tooltipOpen ?
@@ -38,13 +37,12 @@ const DenomSearchBox: FunctionComponent<{type: string}> = ({ type }) => {
                     <input type="text" maxLength={12} placeholder={placeholder}
                        className="placeholder-[#B2BFCD] text-[#c1ccd7] text-tiny sm:text-kindasmaller lg:text-kindasmall
                         group-hover:placeholder-[#c1ccd7] focus:ring-0 border-0 w-full bg-transparent"
-
                     />
                 }
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     onClick={() => setTooltipOpen(!tooltipOpen)}
-                    className="absolute right-4 big:h-6 4k:h-12 lg:h-4 lg:w-4 h-3 w-3 hover:cursor-pointer"
+                    className="hidden sm:block absolute right-4 big:h-6 4k:h-12 lg:h-4 lg:w-4 h-3 w-3 hover:cursor-pointer"
                     fill="#B2BFCD" viewBox="0 0 512 512"
                 >
                     <path d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z"/>
@@ -57,8 +55,8 @@ const DenomSearchBox: FunctionComponent<{type: string}> = ({ type }) => {
 
 const AdvancedButton: FunctionComponent = () => {
     return (
-        <button className="w-fit bg-[#FF6767] p-4 lg:p-5 4k:p-8 big:p-6 gap-2 flex items-center rounded-lg text-white default:text-base big:text-lg 4k:text-2xl desktop:text-kindasmall text-tiny hover:bg-[#ff6f6f]">
-            <svg xmlns="http://www.w3.org/2000/svg" className="lg:w-4 lg:h-4 sm:w-3 sm:h-3 w-2 h-2" fill="white" viewBox="0 0 512 512">
+        <button className="w-fit bg-[#FF6767] p-4 lg:p-5 2xl:p-6 gap-2 flex items-center rounded-lg text-white text-tiny 2xl:text-base hover:bg-[#ff6f6f]">
+            <svg xmlns="http://www.w3.org/2000/svg" className="2xl:h-6 2xl:w-6 sm:w-5 sm:h-5 w-4 h-4" fill="white" viewBox="0 0 512 512">
                 <path d="M304 192v32c0 6.6-5.4 12-12 12h-56v56c0 6.6-5.4 12-12 12h-32c-6.6 0-12-5.4-12-12v-56h-56c-6.6 0-12-5.4-12-12v-32c0-6.6 5.4-12 12-12h56v-56c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v56h56c6.6 0 12 5.4 12 12zm201 284.7L476.7 505c-9.4 9.4-24.6 9.4-33.9 0L343 405.3c-4.5-4.5-7-10.6-7-17V372c-35.3 27.6-79.7 44-128 44C93.1 416 0 322.9 0 208S93.1 0 208 0s208 93.1 208 208c0 48.3-16.4 92.7-44 128h16.3c6.4 0 12.5 2.5 17 7l99.7 99.7c9.3 9.4 9.3 24.6 0 34zM344 208c0-75.2-60.8-136-136-136S72 132.8 72 208s60.8 136 136 136 136-60.8 136-136z"/>
             </svg>
             Advanced
@@ -92,11 +90,11 @@ const SectionHeader: FunctionComponent<{displayToggles: boolean}> = ({ displayTo
     return (
       <>
         {/* Mobile */}
-        <div className="sm:hidden grid grid-rows-2 grid-cols-3 grid-flow-col justify-center items-center w-full mt-4">
-          <div className="row-span-1 col-span-2">
+        <div className="sm:hidden grid grid-rows-2 grid-cols-3 grid-flow-col justify-center items-center w-full mt-4 gap-2">
+          <div className="row-span-1 col-span-2 h-full">
             <DenomSearchBox type={"principal"} />
           </div>
-          <div className="row-span-1 col-span-2">
+          <div className="row-span-1 col-span-2 h-full">
             <DenomSearchBox type={"collateral"} />
           </div>
           <div className="row-span-2 col-span-1 flex justify-center">
@@ -126,7 +124,7 @@ export const ListingsSection: FunctionComponent<{ listings: Listing[], title?: s
       <SectionTitle title={title} />
       <SectionHeader displayToggles={displayToggles} />
         <div className="
-            justify-center items-center w-full my-8
+            justify-center items-center w-full my-4 sm:my-8 2xl:my-10
             grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4
             gap-x-2 sm:gap-x-4 lg:gap-x-6 xl:gap-x-8 2xl:gap-x-10
         ">
