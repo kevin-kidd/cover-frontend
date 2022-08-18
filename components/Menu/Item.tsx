caa7907d45552a857092cd1d250f9dec5296d2a1
Kevin Kidd <harrison.kevin@protonmail.com>
2022-08-18 17:34:17 -0400

    style(Menu): adjust padding and margin for responsive design in Item component

--- components/Menu/Item.tsx
+++ components/Menu/Item.tsx
@@ -12,7 +12,7 @@ const Item: FunctionComponent<{ item: MenuItem, active: boolean }> = ({ item, ac
 
     return (
         <Link href={ disabled ? "" : item.href }>
-            <div className="group flex items-center text-base w-full px-4 my-4 group h-12 hover:cursor-pointer transition duration-150">
+            <div className="group flex items-center text-base w-full px-3 my-3 sm:my-4 sm:px-4 group h-10 sm:h-12 hover:cursor-pointer transition duration-150">
                 <div className={classNames(
                     "mr-2 p-2 h-9 w-9 border rounded-xl",
                     active ? "border-[#7BBD75]" : "border-[#eeeeee]/90 transition duration-150"
