b1c7a905183cefaebf421b999476b481c190f2a4
Kevin Kidd <harrison.kevin@protonmail.com>
2024-08-17 18:51:01 -0400

    style: reformat code and fix import statements in multiple pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,72 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import {useEffect, useRef} from "react";
+import { useEffect, useRef } from "react";
 
-import {useHomeStore} from "../stores/HomeStore";
+import { useHomeStore } from "../stores/HomeStore";
 import exampleListings from "../exampleListings.json";
 
 // Components
 import Menu from "../components/Menu";
 import { ToggleButton } from "../components/Header/ToggleButton";
-import {FaucetWidget, SettingsWidget} from "../components/Header/Widgets";
+import { FaucetWidget, SettingsWidget } from "../components/Header/Widgets";
 import Header from "../components/Header";
-import {ListingsSection} from "../components/Listings/ListingsSection";
-
+import { ListingsSection } from "../components/Listings/ListingsSection";
 
 const ExplorePage: NextPage = () => {
+	const selectedListings = useHomeStore((state) => state.selectedListings);
+	const setSelectedListings = useHomeStore(
+		(state) => state.setSelectedListings,
+	);
+	const featuredListings = useHomeStore((state) => state.featuredListings);
+	const setFeaturedListings = useHomeStore(
+		(state) => state.setFeaturedListings,
+	);
 
-    const selectedListings = useHomeStore((state) => state.selectedListings);
-    const setSelectedListings = useHomeStore((state) => state.setSelectedListings);
-    const featuredListings = useHomeStore((state) => state.featuredListings);
-    const setFeaturedListings = useHomeStore((state) => state.setFeaturedListings);
+	useEffect(() => {
+		setTimeout(() => {
+			setSelectedListings(exampleListings.lend.nft);
+			setFeaturedListings(exampleListings.featured);
+		}, 5000);
+	}, [setSelectedListings, setFeaturedListings]);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setSelectedListings(exampleListings.lend.nft);
-            setFeaturedListings(exampleListings.featured);
-        }, 5000);
-    }, [setSelectedListings, setFeaturedListings]);
+	const items = {
+		left: [<FaucetWidget key="faucet" />],
+		right: [
+			<ToggleButton key="tokenToggle" type="tokenToggle" />,
+			<ToggleButton key="listingToggle" type="listingToggle" />,
+			<SettingsWidget key="settings" page="test" />,
+		],
+	};
 
-    const items = {
-        left: [FaucetWidget({})],
-        right: [ToggleButton({ type: "tokenToggle" }), ToggleButton({ type: "listingToggle" }), SettingsWidget({ page: "test" })]
-    };
+	const containerRef = useRef(null);
 
-    const containerRef = useRef(null);
-
-    return (
-      <>
-          <Head>
-              <title>Explore</title>
-          </Head>
-          <main>
-              <Menu activeTitle={"Explore"} />
-              <div ref={containerRef} className="lg:ml-sidebar w-full lg:w-auto h-screen flex flex-col">
-                  <Header items={items} />
-                  <div className="px-1 sm:px-4 lg:container lg:mx-auto lg:px-20">
-                      <ListingsSection listings={featuredListings} title={"Featured Listings (All)"} displayFilters={false} />
-                      <ListingsSection listings={selectedListings} displayFilters={true} />
-                  </div>
-              </div>
-          </main>
-      </>
-    )
+	return (
+		<>
+			<Head>
+				<title>Explore</title>
+			</Head>
+			<main>
+				<Menu activeTitle={"Explore"} />
+				<div
+					ref={containerRef}
+					className="lg:ml-sidebar w-full lg:w-auto h-screen flex flex-col"
+				>
+					<Header items={items} />
+					<div className="px-1 sm:px-4 lg:container lg:mx-auto lg:px-20">
+						<ListingsSection
+							listings={featuredListings}
+							title={"Featured Listings (All)"}
+							displayFilters={false}
+						/>
+						<ListingsSection
+							listings={selectedListings}
+							displayFilters={true}
+						/>
+					</div>
+				</div>
+			</main>
+		</>
+	);
 };
 
-export default ExplorePage
+export default ExplorePage;
--- pages/my-page.tsx
+++ pages/my-page.tsx
@@ -2,53 +2,61 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Menu from "../components/Menu";
 import Header from "../components/Header";
-import {ListingsSection} from "../components/Listings/ListingsSection";
-import {useHomeStore} from "../stores/HomeStore";
-import {useEffect} from "react";
+import { ListingsSection } from "../components/Listings/ListingsSection";
+import { useHomeStore } from "../stores/HomeStore";
+import { useEffect } from "react";
 import exampleListings from "../exampleListings.json";
 import { useRouter } from "next/router";
-import {FaucetWidget, SettingsWidget} from "../components/Header/Widgets";
-import {useWalletStore} from "../stores/WalletStore";
-
+import { FaucetWidget, SettingsWidget } from "../components/Header/Widgets";
+import { useWalletStore } from "../stores/WalletStore";
 
 const MyPage: NextPage = () => {
+	const listings = useHomeStore((state) => state.selectedListings);
+	const setSelectedListings = useHomeStore(
+		(state) => state.setSelectedListings,
+	);
+	const client = useWalletStore((state) => state.client);
+	const router = useRouter();
 
-    const listings = useHomeStore((state) => state.selectedListings);
-    const setSelectedListings = useHomeStore((state) => state.setSelectedListings);
-    const client = useWalletStore((state) => state.client);
-    const router = useRouter();
+	useEffect(() => {
+		if (client === undefined && typeof window !== "undefined") {
+			router.push("/");
+		}
+		setTimeout(() => {
+			setSelectedListings(exampleListings.lend.nft);
+		}, 5000);
+	}, [setSelectedListings, router, client]);
 
-    useEffect(() => {
-        if(client === undefined && typeof window !== undefined) {
-            router.push("/");
-        }
-        setTimeout(() => {
-            setSelectedListings(exampleListings.lend.nft);
-        }, 5000);
-    }, [setSelectedListings, router, client]);
+	const items = {
+		left: [<FaucetWidget key="faucet" />],
+		right: [<SettingsWidget key="settings" page="test" />],
+	};
 
-    const items = {
-        left: [FaucetWidget({})],
-        right: [SettingsWidget({ page: "test" })]
-    };
-
-    return (
-      <>
-          <Head>
-              <title>My Page</title>
-          </Head>
-          <main className="relative h-screen">
-              <Menu activeTitle={"My Page"} />
-              <div className="lg:ml-sidebar w-full lg:w-auto h-screen flex flex-col">
-                  <Header items={items} />
-                  <div className="px-1 sm:px-4 lg:container lg:mx-auto lg:px-20">
-                      <ListingsSection listings={listings} title={"My Listings (All)"} displayFilters={false} />
-                      <ListingsSection listings={listings} title={"Listings With My Positions"} displayFilters={false} />
-                  </div>
-              </div>
-          </main>
-      </>
-    )
+	return (
+		<>
+			<Head>
+				<title>My Page</title>
+			</Head>
+			<main className="relative h-screen">
+				<Menu activeTitle={"My Page"} />
+				<div className="lg:ml-sidebar w-full lg:w-auto h-screen flex flex-col">
+					<Header items={items} />
+					<div className="px-1 sm:px-4 lg:container lg:mx-auto lg:px-20">
+						<ListingsSection
+							listings={listings}
+							title={"My Listings (All)"}
+							displayFilters={false}
+						/>
+						<ListingsSection
+							listings={listings}
+							title={"Listings With My Positions"}
+							displayFilters={false}
+						/>
+					</div>
+				</div>
+			</main>
+		</>
+	);
 };
 
-export default MyPage
+export default MyPage;
