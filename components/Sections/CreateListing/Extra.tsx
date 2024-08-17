2d43f45bc8586791c06197428fb15e1129ca3f3a
Kevin Kidd <harrison.kevin@protonmail.com>
2024-08-17 18:50:36 -0400

    style: format code and update imports in CreateListing components

--- components/Sections/CreateListing/Extra.tsx
+++ components/Sections/CreateListing/Extra.tsx
@@ -1,131 +1,157 @@
-import React, {FunctionComponent, ReactElement} from "react";
-import {useCreateListingStore} from "../../../stores/CreateListingStore";
-import {Transition} from "@headlessui/react";
-import {round} from "../../../func/helper";
-import {usePersistentStore} from "../../../stores/PersistentStore";
+import React, { type FunctionComponent, type ReactElement } from "react";
+import { useCreateListingStore } from "../../../stores/CreateListingStore";
+import { Transition } from "@headlessui/react";
+import { round } from "../../../func/helper";
+import { usePersistentStore } from "../../../stores/PersistentStore";
 
 export const TermsCard: FunctionComponent = () => {
+	const createListingState = useCreateListingStore((state) => state);
+	const misc = createListingState.misc;
 
-    const createListingState = useCreateListingStore((state) => state);
-    let misc = createListingState.misc;
+	const createListingToggle = usePersistentStore(
+		(state) => state.config.toggles.createListingToggle,
+	);
 
-    const createListingToggle = usePersistentStore((state) => state.config.toggles.createListingToggle);
+	const borrowingTerms: ReactElement = (
+		<>
+			You agree to pay the lender
+			<span className="font-semibold">
+				{` ${createListingState.principal.amount}`}
+				{createListingState.principal.type === "snip20"
+					? ` ${createListingState.principal.token.details.name} `
+					: "Collection Name"}
+			</span>
+			(principal) + up to
+			<span className="font-semibold">
+				{` ${round(
+					createListingState.principal.amount *
+						(createListingState.returnAmount.percentage / 100),
+					2,
+				).toFixed(2)}`}{" "}
+				{createListingState.principal.type === "snip20"
+					? createListingState.principal.token.details.name
+					: "Collection Name"}
+			</span>{" "}
+			(interest) in at most{" "}
+			<span className="font-semibold">
+				{createListingState.duration.days} days
+			</span>
+			. The interest owed is applied in hourly increments. If you fail to repay
+			the loan, you will lose your collateral.
+		</>
+	);
 
-    const borrowingTerms: ReactElement = (
-        <>
-            You agree to pay the lender
-            <span className="font-semibold">
-                { " " + createListingState.principal.amount}
-                { createListingState.principal.type === "snip20" ? ` ${createListingState.principal.token.details.name} ` : "Collection Name" }
-            </span>
-            (principal) + up to
-            <span className="font-semibold">
-                {" " +
-                round(
-                    createListingState.principal.amount * (createListingState.returnAmount.percentage / 100),
-                    2
-                ).toFixed(2)
-                } { createListingState.principal.type === "snip20" ? createListingState.principal.token.details.name : "Collection Name" }
-            </span> (interest) in at most <span className="font-semibold">{ createListingState.duration.days } days</span>.
-            The interest owed is applied in hourly increments. If you fail to repay the loan, you will lose your collateral.
-        </>
-    );
+	const lendingTerms: ReactElement = (
+		<>
+			You are offering to lend
+			<span className="font-semibold">
+				{` ${createListingState.principal.amount}`}
+				{createListingState.principal.type === "snip20"
+					? ` ${createListingState.principal.token.details.name}`
+					: "Collection Name"}
+			</span>
+			. The borrower must return
+			<span className="font-semibold">
+				{` ${createListingState.principal.amount}`}
+				{createListingState.principal.type === "snip20"
+					? ` ${createListingState.principal.token.details.name}`
+					: "Collection Name"}
+			</span>{" "}
+			(principal) + up to
+			<span className="font-semibold">
+				{` ${round(
+					createListingState.principal.amount *
+						(createListingState.returnAmount.percentage / 100),
+					2,
+				).toFixed(2)}`}{" "}
+				{createListingState.principal.type === "snip20"
+					? createListingState.principal.token.details.name
+					: "Collection Name"}
+			</span>{" "}
+			(interest) in at most{" "}
+			<span className="font-semibold">
+				{createListingState.duration.days} days
+			</span>
+			. If they fail to return the principal + interest owed, you will receive
+			the collateral.
+		</>
+	);
 
-    const lendingTerms: ReactElement = (
-        <>
-            You are offering to lend
-            <span className="font-semibold">
-                { " " + createListingState.principal.amount}
-                { createListingState.principal.type === "snip20" ? ` ${createListingState.principal.token.details.name}` : "Collection Name" }
-            </span>.
-            The borrower must return
-            <span className="font-semibold">
-                { " " + createListingState.principal.amount}
-                { createListingState.principal.type === "snip20" ? ` ${createListingState.principal.token.details.name}` : "Collection Name" }
-            </span> (principal) + up to
-            <span className="font-semibold">
-                {" " +
-                round(
-                    createListingState.principal.amount * (createListingState.returnAmount.percentage / 100),
-                    2
-                ).toFixed(2)
-                } { createListingState.principal.type === "snip20" ? createListingState.principal.token.details.name : "Collection Name" }
-            </span> (interest) in at most <span className="font-semibold">{ createListingState.duration.days } days</span>.
-            If they fail to return the principal + interest owed, you will receive the collateral.
-        </>
-    );
+	return (
+		<Transition
+			show={misc.step >= 3}
+			enter="transition-opacity duration-300"
+			enterFrom="opacity-0"
+			enterTo="opacity-100"
+			leave="transition-opacity duration-300"
+			leaveFrom="opacity-100"
+			leaveTo="opacity-0"
+		>
+			<div className="bg-[#1A2128] w-full max-w-md mx-auto rounded-lg px-8 py-6 flex flex-col items-center relative">
+				<h5 className="absolute bottom-2 right-3 text-gray-400">Step 3</h5>
+				<h2 className="card-title pb-5">Terms & Conditions</h2>
 
-    return (
-        <Transition
-            show={misc.step >= 3}
-            enter="transition-opacity duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-        >
-            <div className="bg-[#1A2128] w-full max-w-md mx-auto rounded-lg px-8 py-6 flex flex-col items-center relative">
-                <h5 className="absolute bottom-2 right-3 text-gray-400">Step 3</h5>
-                <h2 className="card-title pb-5">Terms & Conditions</h2>
+				<p className="text-white w-full text-center text-sm mb-2">
+					{createListingToggle === "Borrowing" ? (
+						<>{borrowingTerms}</>
+					) : (
+						<>{lendingTerms}</>
+					)}
+				</p>
 
-                <p className="text-white w-full text-center text-sm mb-2">
-                    { createListingToggle === "Borrowing" ?
-                        <>
-                            { borrowingTerms }
-                        </>
-                        :
-                        <>
-                            { lendingTerms }
-                        </>
-                    }
-                </p>
-
-                <div className="form-control">
-                    <label className="cursor-pointer label">
-                        <input type="checkbox" checked={misc.signedTerms} onChange={() => misc.setSignedTerms(!misc.signedTerms)}
-                               className="checkbox checkbox-accent bg-transparent focus:ring-0 focus:ring-offset-0 focus:outline-none" />
-                        <span className="label-text ml-2 text-white text-sm">I agree to the terms</span>
-                    </label>
-                </div>
-
-            </div>
-        </Transition>
-    )
+				<div className="form-control">
+					<label className="cursor-pointer label">
+						<input
+							type="checkbox"
+							checked={misc.signedTerms}
+							onChange={() => misc.setSignedTerms(!misc.signedTerms)}
+							className="checkbox checkbox-accent bg-transparent focus:ring-0 focus:ring-offset-0 focus:outline-none"
+						/>
+						<span className="label-text ml-2 text-white text-sm">
+							I agree to the terms
+						</span>
+					</label>
+				</div>
+			</div>
+		</Transition>
+	);
 };
 
 export const ApproveCard: FunctionComponent = () => {
+	const misc = useCreateListingStore((state) => state.misc);
 
-    const misc = useCreateListingStore((state) => state.misc);
-
-    return (
-        <Transition
-            show={misc.signedTerms && misc.step >= 4}
-            enter="transition-opacity duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-        >
-            <div className="bg-[#1A2128] w-full h-full max-w-md mx-auto rounded-lg px-10 py-6 flex flex-col items-center relative">
-                <h5 className="absolute bottom-2 right-3 text-gray-400">Step 4</h5>
-                <h2 className="card-title">Approve Listing</h2>
-                <div className="flex flex-col my-auto">
-                    <h4>Are you sure you want to create this listing?</h4>
-                    <div className="flex justify-between mx-auto w-1/2 mt-6">
-                        <button
-                            onClick={misc.reset}
-                            className="py-2 px-4 bg-transparent border-[#D45E5E] border rounded-lg hover:text-white duration-200 transition"
-                        >
-                            No
-                        </button>
-                        <button className="py-2 px-4 bg-transparent border-[#5AAD52] border rounded-lg hover:text-white duration-200 transition">
-                            Yes
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </Transition>
-    )
+	return (
+		<Transition
+			show={misc.signedTerms && misc.step >= 4}
+			enter="transition-opacity duration-300"
+			enterFrom="opacity-0"
+			enterTo="opacity-100"
+			leave="transition-opacity duration-300"
+			leaveFrom="opacity-100"
+			leaveTo="opacity-0"
+		>
+			<div className="bg-[#1A2128] w-full h-full max-w-md mx-auto rounded-lg px-10 py-6 flex flex-col items-center relative">
+				<h5 className="absolute bottom-2 right-3 text-gray-400">Step 4</h5>
+				<h2 className="card-title">Approve Listing</h2>
+				<div className="flex flex-col my-auto">
+					<h4>Are you sure you want to create this listing?</h4>
+					<div className="flex justify-between mx-auto w-1/2 mt-6">
+						<button
+							type="button"
+							onClick={misc.reset}
+							className="py-2 px-4 bg-transparent border-[#D45E5E] border rounded-lg hover:text-white duration-200 transition"
+						>
+							No
+						</button>
+						<button
+							type="button"
+							className="py-2 px-4 bg-transparent border-[#5AAD52] border rounded-lg hover:text-white duration-200 transition"
+						>
+							Yes
+						</button>
+					</div>
+				</div>
+			</div>
+		</Transition>
+	);
 };
--- components/Sections/CreateListing/index.tsx
+++ components/Sections/CreateListing/index.tsx
@@ -1,61 +1,63 @@
-import React, {FunctionComponent, useEffect, useRef} from "react";
-import {usePersistentStore} from "../../../stores/PersistentStore";
-import {useCreateListingStore} from "../../../stores/CreateListingStore";
-import {CollateralCard} from "./CollateralCard";
-import {PrincipalCard} from "./PrincipalCard";
-import {ApproveCard, TermsCard} from "./Extra";
+import React, { type FunctionComponent, useEffect, useRef } from "react";
+import { usePersistentStore } from "../../../stores/PersistentStore";
+import { useCreateListingStore } from "../../../stores/CreateListingStore";
+import { CollateralCard } from "./CollateralCard";
+import { PrincipalCard } from "./PrincipalCard";
+import { ApproveCard, TermsCard } from "./Extra";
 
 const CreateListing: FunctionComponent = () => {
+	const createListingToggle = usePersistentStore(
+		(state) => state.config.toggles.createListingToggle,
+	);
+	const createListingState = useCreateListingStore((state) => state);
+	const misc = createListingState.misc;
 
-    const createListingToggle = usePersistentStore((state) => state.config.toggles.createListingToggle);
-    const createListingState = useCreateListingStore((state) => state);
-    const misc = createListingState.misc;
+	const usePrevious = (value) => {
+		const ref = useRef();
+		useEffect(() => {
+			ref.current = value;
+		});
+		return ref.current;
+	};
 
-    const usePrevious = (value) => {
-        const ref = useRef();
-        useEffect(() => {
-            ref.current = value;
-        });
-        return ref.current;
-    };
+	const prevToggleValue = usePrevious(createListingToggle);
 
-    const prevToggleValue = usePrevious(createListingToggle);
+	useEffect(() => {
+		if (createListingToggle !== prevToggleValue) misc.reset();
+	}, [createListingToggle, misc.reset, prevToggleValue]);
 
-    useEffect(() => {
-        if(createListingToggle !== prevToggleValue) misc.reset();
-    }, [createListingToggle]);
+	useEffect(() => {
+		const duration = createListingState.duration.days;
+		const principalAmount = createListingState.principal.amount;
+		const collateralAmount = createListingState.collateral.amount;
+		if (
+			principalAmount !== -1 &&
+			principalAmount !== 0 &&
+			duration !== -1 &&
+			duration !== 0 // TODO -- add check for collateral amount
+		) {
+			if (collateralAmount !== -1 && collateralAmount !== 0) {
+				if (misc.signedTerms) {
+					misc.setStep(4);
+				} else {
+					misc.setStep(3);
+				}
+			} else {
+				misc.setStep(2);
+			}
+		} else {
+			misc.setStep(1);
+		}
+	}, [createListingState, misc.setStep, misc.signedTerms]);
 
-    useEffect(() => {
-        let duration = createListingState.duration.days;
-        let principalAmount = createListingState.principal.amount;
-        let collateralAmount = createListingState.collateral.amount;
-        if(
-            principalAmount !== -1 && principalAmount !== 0 &&
-            duration !== -1 && duration !== 0 // TODO -- add check for collateral amount
-        ) {
-            if(collateralAmount !== -1 && collateralAmount !== 0) {
-                if(misc.signedTerms) {
-                    misc.setStep(4);
-                } else {
-                    misc.setStep(3);
-                }
-
-            } else {
-                misc.setStep(2);
-            }
-        } else {
-            misc.setStep(1);
-        }
-    }, [createListingState]);
-
-    return (
-        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-x-8 xl:gap-x-4 gap-y-6">
-            <PrincipalCard />
-            <CollateralCard />
-            <TermsCard />
-            <ApproveCard />
-        </div>
-    );
+	return (
+		<div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-x-8 xl:gap-x-4 gap-y-6">
+			<PrincipalCard />
+			<CollateralCard />
+			<TermsCard />
+			<ApproveCard />
+		</div>
+	);
 };
 
 export default CreateListing;
