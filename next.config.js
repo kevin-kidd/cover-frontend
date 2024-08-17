223f558f66cba1993c4627c3a12a21ffc3dc3415
Kevin Kidd <harrison.kevin@protonmail.com>
2024-08-17 18:49:55 -0400

    style: fix formatting issues in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["res.cloudinary.com", "stashhappstorage.blob.core.windows.net"]
-  },
-  experimental: { images: { allowFutureImage: true } }
+	reactStrictMode: true,
+	images: {
+		domains: ["res.cloudinary.com", "stashhappstorage.blob.core.windows.net"],
+	},
 };
