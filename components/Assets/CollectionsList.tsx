2594382bc7a8276c3167ca21123d15011332fd3e
Kevin Kidd <harrison.kevin@protonmail.com>
2024-08-17 18:50:10 -0400

    refactor(Assets): update imports and improve code readability

--- components/Assets/CollectionsList.tsx
+++ components/Assets/CollectionsList.tsx
@@ -1,22 +1,20 @@
-import {FunctionComponent} from "react";
-import {useAssetStore} from "../../stores/AssetStore";
-import {CollectionCard} from "./CollectionCard";
-
+import type { FunctionComponent } from "react";
+import { useAssetStore } from "../../stores/AssetStore";
+import { CollectionCard } from "./CollectionCard";
 
 export const CollectionsList: FunctionComponent = () => {
+	const savedCollections = useAssetStore(
+		(state) => state.assets.snip721.collections,
+	);
 
-    const savedCollections = useAssetStore((state) => state.assets.snip721.collections);
-
-    return (
-        <div className="w-full flex justify-center mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-            {
-                savedCollections.map((collection) => (
-                    <CollectionCard
-                        key={`collection_card-${collection.details.name}`}
-                        collection={collection}
-                    />
-                ))
-            }
-        </div>
-    )
+	return (
+		<div className="w-full justify-center mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+			{savedCollections.map((collection) => (
+				<CollectionCard
+					key={`collection_card-${collection.details.name}`}
+					collection={collection}
+				/>
+			))}
+		</div>
+	);
 };
